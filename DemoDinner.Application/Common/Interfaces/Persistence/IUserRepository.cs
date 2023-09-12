aba296874d5be05c0dde1343eb32cadeef259c61
dzson <dzson.gameplay@gmail.com>
2023-09-12 12:09:57 +0200

    add user repository interface

---
+++ DemoDinner.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using DemoDinner.Domain.Entities;
+
+namespace DemoDinner.Application.Common.Interfaces.Persistence;
+
+public interface IUserRepository
+{
+    User? GetUserByEmail(string email);
+
+    void Add(User user);
+}
--- DemoDinner.Application/Services/Authentication/AuthenticationService.cs
+++ DemoDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,23 +1,41 @@
 ﻿using DemoDinner.Application.Common.Interfaces.Authentication;
+using DemoDinner.Application.Common.Interfaces.Persistence;
+using DemoDinner.Domain.Entities;
 
 namespace DemoDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
 
-    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
     {
         _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        Guid id = Guid.NewGuid();
-        string token = _jwtTokenGenerator.GenerateToken(id, firstName, lastName);
+        if (_userRepository.GetUserByEmail(email) is not null)
+        {
+            throw new Exception("User with given email already exists.");
+        }
+
+        User user = new()
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            Password = password
+        };
+
+        _userRepository.Add(user);
+
+        string token = _jwtTokenGenerator.GenerateToken(user.Id, firstName, lastName);
 
         return new AuthenticationResult(
-            id,
+            user.Id,
             firstName,
             lastName, 
             email,
@@ -26,6 +44,23 @@ public class AuthenticationService : IAuthenticationService
 
     public AuthenticationResult Login(string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), "firstName", "lastName", email, "token");
+        if (_userRepository.GetUserByEmail(email) is not User user)
+        {
+            throw new Exception("Wrong email or password");
+        }
+
+        if (user.Password != password)
+        {
+            throw new Exception("Wrong email or password");
+        }
+
+        string token = _jwtTokenGenerator.GenerateToken(user.Id, user.FirstName, user.LastName);
+
+        return new AuthenticationResult(
+            user.Id,
+            user.FirstName,
+            user.LastName, 
+            email,
+            token);
     }
 }
